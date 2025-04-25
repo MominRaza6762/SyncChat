18d270e60150046a0ed59bbcd77c2ed414b2bb4a
Momin Raza <135518733+MominRaza6762@users.noreply.github.com>
2025-04-25 19:15:48 +0500

    Update socket.jsx
    update socket url

--- client/src/services/socket.jsx
+++ client/src/services/socket.jsx
@@ -1,15 +1,10 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://192.168.18.64:5000/";
-
-
-
+const SOCKET_URL = "https://sync-chat-server-beryl.vercel.app";
 export const socket = io(SOCKET_URL,{withCredentials:true});
-
 export const connectSocket = () => {
     socket.connect();
   };
-
 export const disconnectSocket = () => {
     socket.disconnect();
   };
